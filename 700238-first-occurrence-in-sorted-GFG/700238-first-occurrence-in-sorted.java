807ec78e9e9c458c31138ccfa341f3815b6603aa
Grizzy <grishmashah598@gmail.com>
2026-07-30 22:43:14 +0530

    Added solution - Time: O(log n), Space: O(1)

--- 700238-first-occurrence-in-sorted-GFG/700238-first-occurrence-in-sorted.java
+++ 700238-first-occurrence-in-sorted-GFG/700238-first-occurrence-in-sorted.java
@@ -1,10 +1,21 @@
 class Solution {
     public int firstSearch(int[] arr, int k) {
         int first = -1;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == k){
-                first =  i;
-                break;
+        int left = 0; 
+        int right = arr.length-1;
+        
+        while(left<=right){
+            int mid = left + (right - left)/2;
+            
+            if(arr[mid] == k){
+                first = mid;
+                right = mid -1;
+            }
+            else if(arr[mid] > k){
+                right = mid -1;
+            }
+            else{
+                left = mid +1;;
             }
         }
         return first;
